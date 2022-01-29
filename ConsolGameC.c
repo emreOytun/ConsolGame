8abd9332589700c904cc86d2aecee03fd60f92b8
Emre Oytun <74710437+showtime58@users.noreply.github.com>
2022-01-29 23:09:09 +0300

    ~

--- ConsolGameC.c
+++ ConsolGameC.c
@@ -1,8 +1,14 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define UP 0
 #define DOWN 1
+#define NO 0
+#define YES 1
+
+typedef enum{
+	right=1,down=2,left=3,up=4
+}directions;
 
 void getCommand(char command[]);
 int getArraySize(char **commandPointer);
@@ -13,143 +19,130 @@ int main(){
 	char command[commandSize];
 	char *commandPointer=command;
 	
-	char directions[4][5]={"Up","Right","Down","Left"};
-	int directionPointer=1; //car direction as default, points right
-	int brush = UP; // brush position as defauult
-	int x=1,y=1; // car's positions as default
+	directions direction = right; // default direction
+	int brush = UP; // default brush status
+	int x=1,y=1; // default position
 	
-	char **commandPtrPtr = &commandPointer; 
-	getCommand(command); // Displays menu, gets the command, and checks it.
+	getCommand(command); // displays menu, gets the command, and checks it.
 	
-	int arraySize = getArraySize(commandPtrPtr); // gets the arraySize, and increments commandPointer for it to point next command btw;
+	int arraySize = getArraySize(&commandPointer); // gets the arraySize, and increments commandPointer to point next command
 	printf("Array Size: %d\n",arraySize); // **********************
-	char array[arraySize+2][arraySize+2]; // creates an array whose size is arraySize+2 because we have borders.
+	char board[arraySize+2][arraySize+2]; // creates an array whose size is arraySize+2 because we have borders.
 	int i;
 	for(i=0;i<arraySize+2;i++){
 		int j;
 		for(j=0;j<arraySize+2;j++){
-			if(i==0 || i==arraySize+1){ // Puts * in first line and last line
-				array[i][j]='*';
+			if(i==0 || i==arraySize+1){ // puts * in first line and last line
+				board[i][j]='*';
 			}
-			else if( j==0 || j==arraySize+1){ //Puts * in first column and last column
-				array[i][j]='*';
+			else if( j==0 || j==arraySize+1){ // puts * in first column and last column
+				board[i][j]='*';
 			}
 			else{
-				array[i][j]=' ';
+				board[i][j]=' ';
 			} 	
 		}
 	}
 
-	int ext=0;
-	while(!ext){
+	int ext=NO;
+	while(ext!=YES){
 	  while(*commandPointer != '\0'){
 		char next_command = *commandPointer;
 		printf("next_command: %c\n",*commandPointer); //**************************
 		if(next_command=='1'){
 			brush = DOWN;
-			printf("brush state: %d\n",brush); /*************************/
 		}
 		if(next_command=='2'){
 			brush = UP;
 		}
 		if(next_command=='3'){
-			if(directionPointer==3){ // sag sinirda ise
-				directionPointer=0; // sol uca git, basa don
+			if(direction==up){ // sag sinirda ise
+				direction = right; // sol uca git, basa don
 			}
-			else directionPointer++;
+			else direction++;
 		}
 		if(next_command=='4'){
-			if(directionPointer==0){ // sol sinirda ise 
-				directionPointer=3; // sag uca git, sona git
+			if(direction==right){ // sol sinirda ise 
+				direction = up; // sag uca git, sona git
 			}
-			else directionPointer--;
-			printf("direction: %s\n",directions[directionPointer]); // *******************************
+			else direction--;
+			printf("direction: %s\n",direction); // *******************************
 		}
 		if(next_command=='5'){
-			int squareNum;
-			if(*(commandPointer+1)=='_'){
-				commandPointer+=2;
-	/*	squareNum=atoi(*commandPointer); //That's false, because atoi expects a string. It does not convert a char. */
-				squareNum = *commandPointer - '0'; // We can use that instead.
-			}
-			else{
-				squareNum=1;
-			}
+			int squareNum = getSquareNum(&commandPointer);
 			printf("squareNum: %d\n",squareNum); //************************************
-			char direction = directions[directionPointer][0];
-			if( direction=='U' ){
+			if( direction==up ){
 				int k;
 				for(k=0;k<squareNum;k++){
 					x--;
-					if( x == 0){ // x'i azalttim, border kontrolu yaptim.
+					if( x == 0){		    // check if player is in the border 
 						x=arraySize; 
 					}
 					if(brush==DOWN){
-						array[x][y]='*';
+						board[x][y]='*';
 					}
 				}	
 			}
-			if( direction=='D' ){
+			if( direction==down ){
 				int k;
 				for(k=0;k<squareNum;k++){
 					x++;
-					if( x == arraySize+1 ){
+					if( x == arraySize+1 ){	// check if player is in the border 
 						x=1;
 					}
 					if(brush==DOWN){
-						array[x][y]='*';
+						board[x][y]='*';
 					}
 				}
 			}
-			if( direction=='R'){
+			if( direction==right ){
 				int k;
 				for(k=0;k<squareNum;k++){
 					y++;
-					if( y==arraySize+1 ){
+					if( y==arraySize+1 ){ 	// check if player is in the border 
 						y=1;
 					}
 					if(brush==DOWN){
-						array[x][y]='*';
+						board[x][y]='*';
 					}
 				}
 			}
-			if( direction=='L' ){
+			if( direction==left ){
 				int k;
 				for(k=0;k<squareNum;k++){
 					y--;
-					if(y==0){
+					if(y==0){ 				// check if player is in the border 
 						y=arraySize;
 					}
 					if(brush==DOWN){
-						array[x][y]='*';
+						board[x][y]='*';
 					}
 				}
 			}	
 		}
-		if(next_command=='7'){
-			if(directionPointer==3){
-				directionPointer=1;
+		if(next_command=='7'){				// reverse direction
+			if(direction==up){
+				direction==down;
 			}
-			else directionPointer+=2;
+			else direction+=2;
 		}
-		
 		if(next_command=='8'){
 			int i,j;
 			for(i=0;i<arraySize+2;i++){
 				for(j=0;j<arraySize+2;j++){
-					printf("%c",array[i][j]);
+					printf("%c",board[i][j]);
 				}
 				printf("\n");
 			}
 			printf("\n");		
 		}
 		if(next_command=='0'){
-			ext=1;
+			ext=YES;
 		}
 		commandPointer+=2; // In that way, we skip '+' character.
 	  }
 	  // When one command string is done, then get new command.!
-	  strcpy(command,"");
+	  strcpy(command,""); // clear command buffer
 	  getCommand(command);
 	  commandPointer = command;
     }
@@ -172,8 +165,8 @@ void getCommand(char command[]){
 	scanf("%s",command);
 	
 	int i;
-	for(i=0; command[i]!='\0' ;i++){
-		if(command[i]=='+' || command[i]=='_' || ( ('0'<=command[i])&&(command[i]<='8') ) ){
+	for(i=0; command[i]!='\0' ;i++){	// check if input command is in proper format
+		if(command[i]=='+' || command[i]=='_' || ( ('0'<=command[i])&&(command[i]<='8') ) ){ 
 			continue;
 		}
 		else{
@@ -192,10 +185,21 @@ int getArraySize(char **commandPointer){ // ****To increment the adress that a p
 		i++;
 		*commandPointer+=1;  
 	}
-	*commandPointer+=1; // so that commandPointer points the next element directly
+	*commandPointer+=1; 	// so that commandPointer points the next element directly
 	
 	int size=atoi(arraySize);
 	return size;
 }
 
+int getSquareNum(char **commandPointer){
+	int squareNum;
+	if(*((*commandPointer)+1)=='_'){ 
+		*commandPointer+=2;
+		squareNum = **commandPointer - '0'; 
+	}
+	else{					// if command is like ...+5+2+.... means 1 square
+		squareNum=1;
+	}
+	return squareNum;
+}
 
